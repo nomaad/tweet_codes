6acc683fefecb2bd90676ed8495c1859ca5e2ce2
Matthias Zaugg <matzaugg@gmail.com>
2020-08-05 15:48:36 +0200

    Moved coding scheme to component

---
+++ client/src/components/CodeScheme.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <h5>Coding Scheme:</h5>
+      <table class="table table-striped">
+        <tr>
+          <th scope="row">0</th>
+          <td><b>N/A</b></td>
+          <td>The tweet content does not match any of the codes.</td>
+        </tr>
+        <tr>
+          <th scope="row">1</th>
+          <td><b>Solutionist</b></td>
+          <td>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td><b>Covidiot</b></td>
+          <td>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
--- client/src/components/ListTweets.vue
+++ client/src/components/ListTweets.vue
@@ -25,40 +25,23 @@
       </div>
     </div>
 
-    <div class="row">
-      <div class="col-12">
-        <h5>Coding Scheme:</h5>
-        <table class="table table-striped">
-          <tr>
-            <th scope="row">0</th>
-            <td><b>N/A</b></td>
-            <td>The tweet content does not match any of the codes.</td>
-          </tr>
-          <tr>
-            <th scope="row">1</th>
-            <td><b>Solutionist</b></td>
-            <td>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td><b>Covidiot</b></td>
-            <td>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</td>
-          </tr>
-        </table>
-      </div>
-    </div>
+    <CodeScheme msg="Coding Scheme" />
   </div>
 </div>
 </template>
 
 <script>
 import axios from "axios";
+import CodeScheme from "@/components/CodeScheme.vue";
 
 export default {
   data() {
     return {
       tweet: {}
     };
+  },
+  components: {
+    CodeScheme
   },
   methods: {
     loadRandomTweet(){
@@ -77,6 +60,10 @@ export default {
         })
     },
     updateTweet(tweet) {
+      if(tweet.code == null){
+        this.$refs.code.focus();
+        return;
+      }
       let apiURL = `http://localhost:9000/api/update-tweet/${tweet._id}`;
       console.log(tweet)
       axios.post(apiURL, this.tweet).then((res) => {
